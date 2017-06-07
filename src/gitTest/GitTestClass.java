7a635ac5db89c9897ef8f4512367d78a0c8af05d
ochsm <ochsm@192.168.59.27>
2017-06-07 09:19:13 +0200

    Konstruktor mit 2 Parametern eingeführt

--- src/gitTest/GitTestClass.java
+++ src/gitTest/GitTestClass.java
@@ -3,4 +3,12 @@ package gitTest;
 public class GitTestClass {
 	private int val1, val2;
 
+	public GitTestClass(int val1, int val2) {
+		super();
+		this.val1 = val1;
+		this.val2 = val2;
+	}
+	
+	
+
 }
