b940dd6f84fe51bcc562042aa2204679ad86a52d
ochsm <martinm.ochs@fhws.de>
2017-06-07 10:51:17 +0200

    Getter und Setter für V1

--- src/gitTest/GitTestClass.java
+++ src/gitTest/GitTestClass.java
@@ -2,6 +2,17 @@ package gitTest;
 
 public class GitTestClass {
 	private int val1, val2, val3, val4;
+	double v1;
+
+
+	public double getV1() {
+		return v1;
+	}
+
+
+	public void setV1(double v1) {
+		this.v1 = v1;
+	}
 
 
 	public GitTestClass(int val1, int val2, int val3, int val4) {
