8dae9e9a77695086d38ec6d8e97c53758ce7ecef
ochsm <martinm.ochs@fhws.de>
2017-06-07 10:35:04 +0200

    Konstruktor mit 4 Parametern

--- src/gitTest/GitTestClass.java
+++ src/gitTest/GitTestClass.java
@@ -1,14 +1,15 @@
 package gitTest;
 
 public class GitTestClass {
-	private int val1, val2, val3;
+	private int val1, val2, val3, val4;
 
 
-	public GitTestClass(int val1, int val2, int val3) {
+	public GitTestClass(int val1, int val2, int val3, int val4) {
 		super();
 		this.val1 = val1;
 		this.val2 = val2;
 		this.val3 = val3;
+		this.val4 = val4;
 	}
 	
 	
